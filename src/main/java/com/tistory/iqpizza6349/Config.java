28adc14f0390bd1de6c37fac2332d91b91679172
iqpizza6349 <62752667+iqpizza6349@users.noreply.github.com>
2021-11-10 11:18:31 +0900

    Update Config.java

--- src/main/java/com/tistory/iqpizza6349/Config.java
+++ src/main/java/com/tistory/iqpizza6349/Config.java
@@ -4,7 +4,7 @@ public class Config {
 
     public static boolean debug = false;                                                               // 디버깅 용
 
-    public static String TOKEN = "ODg4NDc0NzI1NDE4OTU0Nzky.YUTOkQ.BnflQ0GGrBQG10Jr9zQz405RkIU";                                                                   // 봇 토큰
+    public static String TOKEN = "";                                                                   // 봇 토큰
 
     public static String DEVELOPER = "654344568111890462";                                                               // 봇 개발자
 
@@ -13,11 +13,11 @@ public class Config {
     public static String WEBHOOK_URL = "";
     // 봇 웹 훅 url
 
-    public static String USER_NAME = "root";
-    public static String USER_PASSWORD = "CoffeeMaster";
+    public static String USER_NAME = "";
+    public static String USER_PASSWORD = "";
 
-    public static String CLIENT_ID = "rOJYd9Zv4GOjlIxmSTkl";
-    public static String CLIENT_SECRET = "5A6PiED6HB";
-    public static String API_URL = "https://openapi.naver.com/v1/papago/n2mt";
+    public static String CLIENT_ID = "";
+    public static String CLIENT_SECRET = "";
+    public static String API_URL = "";
 
 }
